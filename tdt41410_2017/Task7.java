bb7ce676cdcf331bd701521420f734886fc50ee5
anhn <anhnd85@gmail.com>
2017-03-05 23:53:59 +0100

    Second commit

---
+++ tdt41410_2017/Task7.java
@@ -0,0 +1,28 @@
+//Feature envy example
+public class Phone {
+   private final String unformattedNumber;
+   public Phone(String unformattedNumber) {
+      this.unformattedNumber = unformattedNumber;
+   }
+   public String getAreaCode() {
+      return unformattedNumber.substring(0,3);
+   }
+   public String getPrefix() {
+      return unformattedNumber.substring(3,6);
+   }
+   public String getNumber() {
+      return unformattedNumber.substring(6,10);
+   }
+}
+
+public class Customer{
+   // ... more code here
+   private Phone mobilePhone;
+   public String getMobilePhoneNumber() {
+      return "(" + 
+         mobilePhone.getAreaCode() + ") " +
+         mobilePhone.getPrefix() + "-" +
+         mobilePhone.getNumber();
+   }
+}
+   
